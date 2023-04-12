ec5e4c16bdccf9e7606e5d98dc2844ceda192452
Matthew Groholski <mgrohols@asu.edu>
2023-04-11 22:38:15 -0700

    Changed main.sh output file

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ number=0
 for i in ./info/*
 do
     string="script$number"
-    echo $i | nohup python3 ./scripts/main.py > ./output/$string.txt &
+    echo $i | nohup python3 ./scripts/main.py > ./output/$string.out &
     ((number++))
 done
 
