d094a3ce92ea8e0e2d1a46e96eb580ff277fce16
Matthew Groholski <mgrohols@asu.edu>
2023-05-14 19:44:51 -0700

    Added new brands and fixed bug with type finding.

--- scripts/scrapertools.py
+++ scripts/scrapertools.py
@@ -20,9 +20,9 @@ USER_AGENTS = [
 REFERER = ["https://www.google.com","https://search.yahoo.com","https://www.bing.com"]
 
 CLOTHING_DICT = {
-    "top": "[Tt]ops?|[- ]Shirts?|[Jj]ersey|[Tt]ees?|[Cc]ardigan|[Bb]lazer|[Ff]lannel|[Ss]weater|[Pp]olo|[Vv]est|[Tt]urtleneck|[Hh]enley|[Pp]opover|[Hh]alf[- ][Zz]ip|[Bb]utton[- ]Down|[Cc]rew ([Nn]eck)?|[Tt]ank$|[Vv]-[Nn]eck",
+    "top": "[Tt]ops?|[- ]Shirts?|[Jj]ersey|[Tt]ees?|[Cc]ardigan|[Bb]lazer|[Ff]lannel|[Ss]weater|[Pp]olo|[Vv]est|[Tt]urtleneck|[Hh]enley|[Pp]opover|[Hh]alf[- ][Zz]ip|[Bb]utton[- ]Down|[Cc]rew( [Nn]eck)?|[Tt]ank$|[Vv]-[Nn]eck",
     "bottom": "[Jj]eans?|[Ss]horts?|[Pp]ants?|[Tt]rouser[s]?|[Jj]ogger[s]?|[Ll]eggings|[Ss]lacks|[Cc]hino|[Hh]igh-[Rr]ise",
-    "underclothing": "[Uu]nderwear|[Bb]oxer|[Bb]rief[s]*|[Tt]hong|[Pp]ant(?:ies|y)|[Bb]ra(?:lette)?|[Cc]orset|[Gg]arter|[Bb]abydoll|[Tt]edd(?:ies|y)",
+    "underclothing": "[Uu]nderwear|[Bb]oxer|[Bb]rief[s]?|[Tt]hong|[Pp]ant(?:ies|y)|[Bb]ra(?:lette)?|[Cc]orset|[Gg]arter|[Bb]abydoll|[Tt]edd(?:ies|y)",
     "shoes": "[Ss]hoes?|[Ss]andals|[Ss]lides|[Bb]oots?|[Ss]neakers?|[Hh]eels?|[Ss]tilettos?|[Ff]latforms?|[Ww]edges?|[Pp]umps?",
     "jacket": "[Jj]acket|[Hh]oodie|[Pp]ullover|[Ss]hacket|[Aa]norak|[Pp]arka|[Bb]omber|[Cc]oat|[Ss]weatshirt",
     "sleepwear": "[Ss]leepwear|[Pp]ajamas?|[Nn]ightie|[Rr]obe|[Ss]lip|[Cc]ami(?:sole)?",
@@ -114,16 +114,12 @@ def getCLOTHING_DICT():
 def getType(string: str):
     string = cleanString(string)
 
-    p = re.findall(getCLOTHING_DICT(), string)
+    *_, p = re.finditer(getCLOTHING_DICT(), string)
 
-    if len(p) < 1:
-        return "other"
-
-    lastString = p[len(p) - 1]
-
-    for i in CLOTHING_DICT:
-        if re.search(CLOTHING_DICT[i], lastString):
-            return i
+    if p:  
+        for i in CLOTHING_DICT:
+            if re.search(CLOTHING_DICT[i], p.group()):
+                return i
 
     return "other"
 
