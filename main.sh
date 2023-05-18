f7ae1688a86932a7f19cd48ac4ad0df665809dad
Matthew Groholski <mgrohols@asu.edu>
2023-05-17 20:34:42 -0700

    Updated main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read groupNumber
+read GROUP
 number=0
 
-for i in ./info/group$groupNumber/*
+for i in ./info/group$GROUP/*
 do
     string="script$number"
     echo $i | nohup python3 ./scripts/main.py > ./output/$string.out &
