f6f965402544d02a14b5624a720745188cda8423
Matthew Groholski <mgrohols@asu.edu>
2023-05-19 07:32:13 -0700

    Updated image resolution

--- scripts/main.py
+++ scripts/main.py
@@ -197,7 +197,8 @@ async def main():
                             if match:
                                 match2 = re.search("[0-9]+", match.group(0))
                                 imageSrc[i] = imageUrl[:match.start()+match2.start()] + "720" + imageUrl[match.start()+match2.end():]
-                                
+                            
+                            imageUrl = imageSrc[i]
                             match = re.search("(\?|&)(h)=[0-9]+&?", imageUrl)
                             if match:
                                 match2 = re.search("[0-9]+", match.group(0))
