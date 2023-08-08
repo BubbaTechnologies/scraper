66eac34f3a80e5c35c5cb84ef79c1eb0f5abb704
Matthew Groholski <mgrohols@asu.edu>
2023-08-08 10:45:59 -0700

    Updated shutdown script and vuori

--- scripts/shutdown.py
+++ scripts/shutdown.py
@@ -8,5 +8,5 @@ import re
 for process in psutil.process_iter():
     if re.search("([Pp]ython|[Cc]hrom)", process.name().lower()):
         print("Killing process {0}".format(str(process.pid)))
-        subprocess.call(["kill", process.pid])
+        subprocess.run(["kill", str(process.pid)])
 
